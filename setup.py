c5153d02af173a017e073997b4c5091c95db1857
dawkaka <yousiph77@gmail.com>
2023-09-19 12:17:36 +0000

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="apitoolkit_django",
-    version="0.1",
+    version="0.1.2",
     packages=find_packages(),
     description='A Django SDK for Apitoolkit integration',
     long_description=long_description,
