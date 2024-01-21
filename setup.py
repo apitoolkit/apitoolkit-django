46e8b6af3aed155ffe2a10b767cb41b3dfa2a134
dawkaka <yousiph77@gmail.com>
2024-01-21 10:19:27 +0000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="apitoolkit-django",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     description='A Django SDK for Apitoolkit integration',
     long_description=long_description,
